71904d49f8c3db43dd8b48e59d86d81b622b1eb6
Sarai Remi Prentice <sarai.prentice@bison.howard.edu>
2025-12-10 22:31:59 +0000

    corrections

--- pipes_processes3.c
+++ pipes_processes3.c
@@ -2,30 +2,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
 
 int main(int argc, char *argv[]) {
-    // Check 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <grep_argument>\n", argv[0]);
         exit(1);
     }
 
-    int pipe1[2];  
-    int pipe2[2];  
-    // Create first pipe
+    int pipe1[2];  // cat -> grep
+    int pipe2[2];  // grep -> sort
+
+    // Create both pipes
     if (pipe(pipe1) == -1) {
         perror("pipe1 failed");
         exit(1);
     }
     
-    // Create second pipe
     if (pipe(pipe2) == -1) {
         perror("pipe2 failed");
         exit(1);
     }
 
-    // Create first child process (grep)
+    // Fork child 1 (cat)
     pid_t pid1 = fork();
     if (pid1 == -1) {
         perror("fork failed");
@@ -33,66 +31,81 @@ int main(int argc, char *argv[]) {
     }
 
     if (pid1 == 0) {
-        // First child process (grep)
+        // Child 1: cat scores
+        // Close unused pipe ends
+        close(pipe1[0]);
+        close(pipe2[0]);
+        close(pipe2[1]);
         
-        // Create second child process (sort) from first child
-        pid_t pid2 = fork();
-        if (pid2 == -1) {
-            perror("fork failed");
-            exit(1);
-        }
-
-        if (pid2 == 0) {
-            // Second child process (sort)
-            
-            // Close unused pipe ends
-            close(pipe1[0]);
-            close(pipe1[1]);
-            close(pipe2[1]);  // Close write end of pipe2
-            
-            // Redirect stdin to read from pipe2
-            dup2(pipe2[0], STDIN_FILENO);
-            close(pipe2[0]);
-            
-            // Execute sort
-            execlp("sort", "sort", NULL);
-            perror("execlp sort failed");
-            exit(1);
-        } else {
-            // First child process (grep)
-            
-            // Close unused pipe ends
-            close(pipe1[1]);  // Close write end of pipe1
-            close(pipe2[0]);  // Close read end of pipe2
-            
-            // Redirect stdin to read from pipe1
-            dup2(pipe1[0], STDIN_FILENO);
-            close(pipe1[0]);
-            
-            // Redirect stdout to write to pipe2
-            dup2(pipe2[1], STDOUT_FILENO);
-            close(pipe2[1]);
-            
-           
-            execlp("grep", "grep", argv[1], NULL);
-            perror("execlp grep failed");
-            exit(1);
-        }
-    } else {
-        
-        close(pipe1[0]);  
-        close(pipe2[0]);  
-        close(pipe2[1]);  
-        
-        
+        // Redirect stdout to pipe1 write end
         dup2(pipe1[1], STDOUT_FILENO);
         close(pipe1[1]);
-        
         
         execlp("cat", "cat", "scores", NULL);
         perror("execlp cat failed");
         exit(1);
     }
+
+    // Fork child 2 (grep)
+    pid_t pid2 = fork();
+    if (pid2 == -1) {
+        perror("fork failed");
+        exit(1);
+    }
+
+    if (pid2 == 0) {
+        // Child 2: grep <argument>
+        // Close unused pipe ends
+        close(pipe1[1]);
+        close(pipe2[0]);
+        
+        // Redirect stdin to pipe1 read end
+        dup2(pipe1[0], STDIN_FILENO);
+        close(pipe1[0]);
+        
+        // Redirect stdout to pipe2 write end
+        dup2(pipe2[1], STDOUT_FILENO);
+        close(pipe2[1]);
+        
+        execlp("grep", "grep", argv[1], NULL);
+        perror("execlp grep failed");
+        exit(1);
+    }
+
+    // Fork child 3 (sort)
+    pid_t pid3 = fork();
+    if (pid3 == -1) {
+        perror("fork failed");
+        exit(1);
+    }
+
+    if (pid3 == 0) {
+        // Child 3: sort
+        // Close unused pipe ends
+        close(pipe1[0]);
+        close(pipe1[1]);
+        close(pipe2[1]);
+        
+        // Redirect stdin to pipe2 read end
+        dup2(pipe2[0], STDIN_FILENO);
+        close(pipe2[0]);
+        
+        execlp("sort", "sort", NULL);
+        perror("execlp sort failed");
+        exit(1);
+    }
+
+    // Parent process
+    // Close all pipe ends (parent doesn't use any)
+    close(pipe1[0]);
+    close(pipe1[1]);
+    close(pipe2[0]);
+    close(pipe2[1]);
+
+    // Wait for all children to prevent zombies
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
 
     return 0;
 }
